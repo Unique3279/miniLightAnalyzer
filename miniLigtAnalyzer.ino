d1923a6f79e4def5492866e4796bb49f7abc5ad3
Unique3279 <goomingi@gmail.com>
2023-06-02 11:29:56 +0900

    V2
    -added secondary stepper
    -added automated reset function

--- miniLigtAnalyzer.ino
+++ miniLigtAnalyzer.ino
@@ -1,30 +1,52 @@
 #include <Stepper.h>   // 이거 왜이럼; // 오 고침
-#define StepperPin1 8
-#define StepperPin2 9
-#define StepperPin3 10
-#define StepperPin4 11
+#define BFS_Pin1 4
+#define BFS_Pin2 5
+#define BFS_Pin3 6
+#define BFS_Pin4 7
+
+#define BFS2_Pin1 8
+#define BFS2_Pin2 9
+#define BFS2_Pin3 10
+#define BFS2_Pin4 11
+
 #define StartBtn 2
+#define ResetBtn 3
 #define LightSensor A0
 
-Stepper BFS(2048, StepperPin1, StepperPin2, StepperPin3, StepperPin4);
+Stepper BFS(2048, BFS_Pin1, BFS_Pin2, BFS_Pin3, BFS_Pin4);    //  2048 = 2pi radian
+Stepper BFS_2(2048, BFS2_Pin1, BFS2_Pin2, BFS2_Pin3, BFS2_Pin4);    //  servo for automated reset
 int len = 0;
-
-void getData(){
-  int cnt = 0;                              // 현재 스텝 횟수(위치) 기록
-  detachInterrupt(0);                       // 버튼 여러번 눌림 방지 위해 인터럽트 중지
-  for(cnt; cnt<=100; ++cnt){                // 구간 100분할
-    BFS.step(len);
-    Serial.print(cnt); Serial.print(" "); Serial.println(analogRead(LightSensor));      
-  }
-  attachInterrupt(0, getData, FALLING);     // 인터럽트 재활성화
-}
+int cnt = 0;
 
 void setup(){
   BFS.setSpeed(20);
+  BFS_2.setSpeed(20);
   pinMode(StartBtn, INPUT_PULLUP);          // 시작버튼 할당
-  attachInterrupt(0, getData, FALLING);     // 시작버튼에 인터럽트 부착
+  attachInterrupt(digitalPinToInterrupt(StartBtn), getData, FALLING);     // 시작버튼에 인터럽트 부착
+  pinMode(ResetBtn, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(ResetBtn), resetPos, FALLING);
   pinMode(LightSensor, INPUT);              // 광센서
   Serial.begin(9600);                       // 통신시작
+}
+
+void getData(){                            
+  detachInterrupt(digitalPinToInterrupt(StartBtn));                       // 버튼 여러번 눌림 방지 위해 인터럽트 중지
+  detachInterrupt(digitalPinToInterrupt(ResetBtn));
+  for(cnt; cnt<=100; ++cnt){                // 구간 100분할
+    BFS.step(len);
+    BFS_2.step(len);
+    Serial.print(cnt); Serial.print(" "); Serial.println(analogRead(LightSensor));      
+  }
+  attachInterrupt(digitalPinToInterrupt(ResetBtn), resetPos, FALLING);
+}
+
+void resetPos(){
+  detachInterrupt(digitalPinToInterrupt(StartBtn));
+  detachInterrupt(digitalPinToInterrupt(ResetBtn));
+  int stepToMove = -1 * cnt * len;
+  BFS.step(stepToMove);
+  BFS_2.step(stepToMove);
+  attachInterrupt(digitalPinToInterrupt(StartBtn), getData, FALLING);
 }
 
 void loop(){
