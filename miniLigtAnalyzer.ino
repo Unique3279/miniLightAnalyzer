edbfe4c62205e003d65054cf920ea77b75c8be1d
junho <60024810+Juno26@users.noreply.github.com>
2023-06-13 15:14:52 +0900

    Update miniLigtAnalyzer.ino
    Update Light Sensor

--- miniLigtAnalyzer.ino
+++ miniLigtAnalyzer.ino
@@ -1,5 +1,8 @@
-#include <Stepper.h>   // 이거 왜이럼; // 오 고침
+#include <Stepper.h>
 #include <math.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_TSL2561_U.h>
 
 #define BFS_Pin1 4
 #define BFS_Pin2 5
@@ -13,30 +16,45 @@
 
 #define StartBtn 2
 #define ResetBtn 3
-#define LightSensor A0
 
 #define PI 3.1415926535897932384626433832795028841971693993751
-#define MAX_STEP 100 // 최대 스텝 수
-#define d 1000 // 회절격자필름 간격. 단위 nm
+#define MAX_STEP 100                        // 최대 스텝 수
+#define d 1000                              // 회절격자필름 간격. 단위 nm
 
 Stepper BFS(2048, BFS_Pin1, BFS_Pin2, BFS_Pin3, BFS_Pin4);    //  2048 = 2pi radian
 Stepper BFS_2(2048, BFS2_Pin1, BFS2_Pin2, BFS2_Pin3, BFS2_Pin4);    //  stepper for automated reset
 
-int len = 2 / (35 * PI) * 2048;                              // 한 스텝에서 도는 각도
-int cnt = 0;                              // 현재 스텝 횟수(위치) 기록
+Adafruit_TSL2561_Unified tsl = Adafruit_TSL2561_Unified(TSL2561_ADDR_FLOAT, 12345);
+sensors_event_t event;
+
+int len = 2 / (35 * PI) * 2048;             // 한 스텝에서 도는 각도
+int cnt = 0;                                // 현재 스텝 횟수(위치) 기록
 
 void setup(){
-  BFS.setSpeed(20);
-  BFS_2.setSpeed(20);
+  BFS.setSpeed(60);
+  BFS_2.setSpeed(60);
 
   pinMode(StartBtn, INPUT_PULLUP);          // 시작버튼 세팅
   attachInterrupt(digitalPinToInterrupt(StartBtn), getData, FALLING);
 
   pinMode(ResetBtn, INPUT_PULLUP);          // 리셋버튼 세팅
-
-  pinMode(LightSensor, INPUT);              // 광센서
   
   Serial.begin(9600);                       // 통신시작
+
+  if(!tsl.begin())
+  {
+    Serial.print("Ooops, no TSL2561 detected ... Check your wiring or I2C ADDR!");
+    while(1);
+  }
+
+  tsl.enableAutoRange(true);            /* Auto-gain ... switches automatically between 1x and 16x */
+  
+  tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_13MS);      /* fast but low resolution */
+  // tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_101MS);  /* medium resolution and speed   */
+  // tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_402MS);  /* 16-bit data but slowest conversions */
+  
+  /* We're ready to go! */
+  Serial.println("Ready.");
 }
 
 void getData(){                            
@@ -46,8 +64,9 @@ void getData(){
   for(cnt; cnt<=MAX_STEP; ++cnt){                // 구간 100분할 후 동작
     BFS.step(len);
     BFS_2.step(len);
-    Serial.print(cnt); Serial.print(" "); Serial.println(analogRead(LightSensor) * calcLIC(cnt));      // 횟수 (tab) 데이터 형식으로 시리얼 출력
-    // F(n)만 곱해둔 상태. 끝나고 파이썬으로 G(lambda_n), H(theta_n) 곱해야 함 << 으윽;
+    tsl.getEvent(&event);
+    Serial.print(cnt); Serial.print(" "); Serial.println(analogRead(event.light) * calcLIC(cnt));      // 횟수 (tab) 데이터 형식으로 시리얼 출력
+    // 파이썬 추가 작업 필요
   }
 
   attachInterrupt(digitalPinToInterrupt(ResetBtn), resetPos, FALLING);    // 리셋 버튼 재활성화
@@ -81,9 +100,6 @@ double calcLIC(int step) {
 }
 
 void loop(){
+  tsl.getEvent(&event);
   delay(100);
-  //  대충 기능 구현 <- 여기에 특별히 뭐 쓸 거 있음...?? << 몰?루
 }
-
-//이제 주노가 해주지 않을까요 <- 는 무슨 안함 ㅅㄱ
-//야이놈아 함수 자료형 안맞추냐
